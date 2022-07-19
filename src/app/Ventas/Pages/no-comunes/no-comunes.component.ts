c7a8bf9a817eb8e343841102e444f12d3c5896a6
Daniel <dinteriano@unah.hn>
2022-07-18 23:03:26 -0600

    Agregan Dialog  primeNG

--- src/app/Ventas/Pages/no-comunes/no-comunes.component.ts
+++ src/app/Ventas/Pages/no-comunes/no-comunes.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [],
+  styles: [
+    `
+      :host ::ng-deep .p-button {
+        margin: 0 0.5rem 0 0;
+        min-width: 10rem;
+      }
+
+      p {
+        margin: 0;
+      }
+
+      .confirmation-content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+
+      :host ::ng-deep .p-dialog .p-button {
+        min-width: 6rem;
+      }
+    `,
+  ],
 })
 export class NoComunesComponent {
+  //i18nSelect
   nombre: string = 'Karina';
   genero: string = 'femenino';
 
@@ -13,4 +36,37 @@ export class NoComunesComponent {
     masculino: 'seleccionado',
     femenino: 'seleccionada',
   };
+
+  //i18nPlural
+  clientes: string[] = ['Ana', 'Lucia', 'Alicia'];
+  clientesMapa = {
+    '=0': 'no tenemos clientes esperando.',
+    '=1': 'tenemos 1 cliente esperando.',
+    other: 'tenemos # clientes esperando.',
+  };
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
+
+  displayModal: boolean = false;
+
+  showModalDialog() {
+    this.displayModal = true;
+  }
+
+  //metodos
+  cambiarPersona() {
+    if (this.genero == 'femenino') {
+      (this.genero = 'masculino'), (this.nombre = 'Josue David');
+    } else {
+      (this.genero = 'femenino'), (this.nombre = 'Ana Lucia');
+    }
+  }
+
+  borrarPersona() {
+    this.clientes.pop();
+  }
 }
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 //primeNG
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
 import { FieldsetModule } from 'primeng/fieldset';
+import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
-  exports: [ButtonModule, CardModule, MenubarModule, FieldsetModule],
+  exports: [
+    ButtonModule,
+    CardModule,
+    MenubarModule,
+    FieldsetModule,
+    DialogModule,
+    InputTextModule,
+  ],
 })
 export class PrimeNGModule {}
